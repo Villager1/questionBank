0747a2b065b5ab943560031ec2b7ef73d2b9b9c9
Villager1 <70864273+Villager1@users.noreply.github.com>
2025-10-27 15:00:11 -0400

    Update and rename biochem3d03test1.js to biochem3h03test2.js

--- banks/biochem3d03test1.js
+++
@@ -1,4 +0,0 @@
-//Bank questions here
-//bankBiochem3d03t1.push(new question("", "", "", "", "", 1, ""));
-
-bankBiochem3d03t1.push(new question("q", "a", "b", "c", "d", 1, "e"));
---
+++ banks/biochem3h03test2.js
@@ -0,0 +1,4 @@
+//Bank questions here
+//bankBiochem3h03t2.push(new question("", "", "", "", "", 1, ""));
+
+bankBiochem3h03t2.push(new question("q", "a", "b", "c", "d", 1, "e"));
