04c2bcb4c1e3671fad7c0f318002bb3956846386
Villager1 <70864273+Villager1@users.noreply.github.com>
2025-10-28 00:46:37 -0400

    Update biochem3h03test2.js

--- banks/biochem3h03test2.js
+++ banks/biochem3h03test2.js
@@ -1,4 +1,15 @@
 //Bank questions here
 //bankBiochem3h03t2.push(new question("", "", "", "", "", 1, ""));
 
-bankBiochem3h03t2.push(new question("q", "a", "b", "c", "d", 1, "e"));
+//Lecture 6A Diabetes Mellitus
+bankBiochem3h03t2.push(new question("", "a", "b", "c", "d", 1, "e"));
+
+//Lecture 6B Diabetes Mellitus
+
+//Lecture 7 Cardiovascular Diseases
+
+//Lecture 8A Complete Blood Count
+
+//Lecture 8B Complete Blood Count
+
+//Lecture 9 Thyroid
