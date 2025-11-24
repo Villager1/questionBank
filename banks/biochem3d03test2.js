4d95b8acf5e1d8570013205308bdf28cdf9c0725
Villager1 <70864273+Villager1@users.noreply.github.com>
2025-11-24 01:27:30 -0500

    Update biochem3d03test2.js
    added lecture 5

--- banks/biochem3d03test2.js
+++ banks/biochem3d03test2.js
@@ -18,10 +18,13 @@ bankBiochem3d03t2.push(new question("What cofactors are required for the PDH com
 bankBiochem3d03t2.push(new question("Which is false about the PDH complex?", "It is active when phosphorylated", "It is activated by PDH phosphatase, and inactivated by PDH kinase", "It is made up of 3 enzymes", "NADH and acetyl-CoA are end-product inhibitors", 1, "PDH is active when dephosphorylated."));
 bankBiochem3d03t2.push(new question("What are allosteric activators and inhibitors of PDH phosphatase?", "___", "___", "Activators: Ca, Mg; Inhibitors: none", "___", 3, "Activators include calcium and magnesium."));
 bankBiochem3d03t2.push(new question("What are allosteric activators and inhibitors of PDH kinase?", "___", "Activators: NADH, acetyl-CoA, ATP; Inhibitors: ADP, pyruvate, Ca, Mg", "___", "___", 2, "Activators include NADH, acetyl-CoA, and ATP, inhibitors include ADP, pyruvate, calcium, and magnesium."));
-bankBiochem3d03t2.push(new question("TCA cycle start---------------------", "", "", "", "", 1, ""));
-bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
-bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
-bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
+bankBiochem3d03t2.push(new question("What are allosteric activators and inhibitors of citrate synthase?", "___", "___", "Activators: none; inhibitors: citrate, NADH, succinyl-CoA", "Activators: acetyl-CoA, oxaloacetate; inhibitors: citrate, NADH, succinyl-CoA", 3, "Inhibitors include citrate, NADH, and succinyl-CoA. Acetyl-CoA and Oxaloacetate are substrates, not allosteric activators."));
+bankBiochem3d03t2.push(new question("What are allosteric activators and inhibitors of isocitrate dehydrogenase?", "Activators: Ca, ADP; inhinbitors: ATP, NADH", "___", "___", "___", 1, "Activators include calcium and ADP, inhibitors include ATP and NADH."));
+bankBiochem3d03t2.push(new question("What are allosteric activators and inhibitors of ɑ-ketoglutarate dehydrogenase?", "___", "Activators: Ca; inhibitors: NADH, succinyl-CoA", "___", "___", 2, "Activators include calcium, inhibitors include NADH and succinyl-CoA."));
+bankBiochem3d03t2.push(new question("Why does high ATP lead to less ATP synthase activity?", "___", "___", "High ATP leads to a higher ATP / ADP ratio, and since ADP is necessary for ATP synthesis, the reaction cannot occur", "___", 3, "ADP is necessary for ATP synthesis, and if the ATP / ADP ratio is to high, it is more difficult for ATP synthase to find an ADP molecule."));
+
+
+//Lecture 6
 bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
 bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
 bankBiochem3d03t2.push(new question("", "", "", "", "", 1, ""));
